99e74ea96e7f5c26c4c099b5ed261f93e60e613b
ogoncharenko <ogoncharenko@interprocom.ru>
2026-03-06 20:47:31 +0300

    fix: Добавлен контроллер для оплат

--- payment-service/src/main/java/ru/goncharenko/payment/controller/PaymentController.java
+++ payment-service/src/main/java/ru/goncharenko/payment/controller/PaymentController.java
@@ -1,11 +1,42 @@
 package ru.goncharenko.payment.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import jakarta.annotation.Generated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import ru.goncharenko.payment.response.ApiBalanceGet200Response;
+import ru.goncharenko.payment.model.Payment;
+import ru.goncharenko.payment.service.AccountService;
 
-@RestController
-public class PaymentController {
-	@GetMapping("/diagnostic/liquibase")
-	public void diagnostic() {
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-03-05T18:56:04.818676400+03:00[GMT+03:00]", comments = "Generator version: 7.20.0")
+@Controller
+@RequestMapping("${openapi.payment.base-path:}")
+@RequiredArgsConstructor
+public class PaymentController implements PaymentApi {
+	private final AccountService service;
+
+	@Override
+	public Mono<ResponseEntity<ApiBalanceGet200Response>> apiBalanceGet(
+			@NotNull @Parameter(name = "userName", description = "Логин пользователя", required = true, in = ParameterIn.QUERY)
+			@Valid @RequestParam(value = "userName") String userName,
+			@Parameter(hidden = true) final ServerWebExchange exchange) {
+		return service.getUserBalance(userName);
+	}
+
+	@Override
+	public Mono<ResponseEntity<String>> apiBalancePost(
+			@Parameter(name = "Payment", required = true) @Valid @RequestBody Payment payment,
+			@Parameter(hidden = true) final ServerWebExchange exchange
+	) {
+		return service.makePayment(payment);
 	}
 }
