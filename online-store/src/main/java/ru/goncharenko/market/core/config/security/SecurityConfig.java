ea29b656c1894875cea9c2e4e02fa7bd5c56bbda
ogoncharenko <ogoncharenko@interprocom.ru>
2026-03-19 20:29:04 +0300

    fix: Добавлена аннотация EnableReactiveMethodSecurity

--- online-store/src/main/java/ru/goncharenko/market/core/config/security/SecurityConfig.java
+++ online-store/src/main/java/ru/goncharenko/market/core/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
@@ -21,6 +22,7 @@ import java.net.URI;
 
 @Configuration
 @EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
 	@Bean
 	public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
