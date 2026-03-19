7142a5e8e20767151875f4b7c69a20a3d15c691c
ogoncharenko <ogoncharenko@interprocom.ru>
2026-03-19 20:29:42 +0300

    feat: Добавлена аутентификация по jwt токену

---
+++ payment-service/src/main/java/ru/goncharenko/payment/config/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package ru.goncharenko.payment.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+	@Bean
+	SecurityWebFilterChain securityFilterChain(ServerHttpSecurity security) {
+		return security
+				.authorizeExchange(exchanges -> exchanges
+						.anyExchange().authenticated()
+				)
+				.oauth2ResourceServer(oauth2 -> oauth2
+						.jwt(jwt -> jwt
+								.jwtAuthenticationConverter(jwtAuthenticationConverter())
+						)
+				)
+				.build();
+	}
+
+	@Bean
+	ReactiveJwtDecoder reactiveJwtDecoder() {
+		return ReactiveJwtDecoders.fromIssuerLocation("http://localhost:8090/realms/master");
+	}
+
+	@Bean
+	ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+		ReactiveJwtAuthenticationConverter converter = new ReactiveJwtAuthenticationConverter();
+		converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+			Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+			if (resourceAccess == null) {
+				return Flux.empty();
+			}
+
+			Map<String, Object> account = (Map<String, Object>) resourceAccess.get("account");
+			if (account == null) {
+				return Flux.empty();
+			}
+
+			List<String> roles = (List<String>) account.get("roles");
+			if (roles == null) {
+				return Flux.empty();
+			}
+
+			return Flux.fromIterable(roles)
+					.map(SimpleGrantedAuthority::new);
+		});
+		return converter;
+	}
+}
