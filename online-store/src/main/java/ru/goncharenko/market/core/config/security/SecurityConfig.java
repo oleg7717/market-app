5fe76b159450a24fe32e999c317412a135dc692c
ogoncharenko <ogoncharenko@interprocom.ru>
2026-03-19 19:31:18 +0300

    fix: Исправлены настройки безопасности

--- online-store/src/main/java/ru/goncharenko/market/core/config/security/SecurityConfig.java
+++ online-store/src/main/java/ru/goncharenko/market/core/config/security/SecurityConfig.java
@@ -6,13 +6,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.web.server.WebSession;
+import reactor.core.publisher.Mono;
 
 import java.net.URI;
 
@@ -31,19 +34,22 @@ public class SecurityConfig {
 						.principal("anonymous")
 						.authorities("ROLE_GUEST")
 				)
-				.formLogin(formLogin -> formLogin
-						.authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/items"))
-				)
+				.formLogin(Customizer.withDefaults())
 				.logout(logout -> logout
 						.logoutUrl("/logout")
-						.logoutSuccessHandler((exchange, authentication) -> {
-							ServerHttpResponse response = exchange.getExchange().getResponse();
-							response.setStatusCode(HttpStatus.FOUND);
-							response.getHeaders().setLocation(URI.create("/items"));
-							return response.setComplete();
-						})
+						.logoutSuccessHandler((exchange, authentication) ->
+								exchange.getExchange().getSession()
+										.flatMap(WebSession::invalidate)
+										.then(Mono.defer(() -> {
+											ServerHttpResponse response = exchange.getExchange().getResponse();
+											response.setStatusCode(HttpStatus.FOUND); // 302 для редиректа
+											response.getHeaders().setLocation(URI.create("/items"));
+											return response.setComplete();
+										}))
+						)
 				)
-				.csrf(ServerHttpSecurity.CsrfSpec::disable)
+				.csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+				)
 				.build();
 	}
 
